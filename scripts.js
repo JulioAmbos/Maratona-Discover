b6caee88d3a0c2a8ed430ef31d839cbbef08316a
Julio Moraes <72757371+JulioAmbos@users.noreply.github.com>
2021-02-05 13:35:16 -0300

    Acrescentado recurso de exportação do extrato para excel
    Acrescentado recurso de exportação do extrato para excel.

--- scripts.js
+++ scripts.js
@@ -213,4 +213,34 @@ const App = {
   },
 }
 
+function retira_acentos(str) 
+{
+
+    com_acento = "ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝŔÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿŕ";
+
+sem_acento = "AAAAAAACEEEEIIIIDNOOOOOOUUUUYRsBaaaaaaaceeeeiiiionoooooouuuuybyr";
+    novastr="";
+    for(i=0; i<str.length; i++) {
+        troca=false;
+        for (a=0; a<com_acento.length; a++) {
+            if (str.substr(i,1)==com_acento.substr(a,1)) {
+                novastr+=sem_acento.substr(a,1);
+                troca=true;
+                break;
+            }
+        }
+        if (troca==false) {
+            novastr+=str.substr(i,1);
+        }
+    }
+    return novastr;
+}
+
+function ExportToExcel() {
+    var htmltable = document.querySelector('#data-table');
+    var html = htmltable.outerHTML;
+    window.open('data:application/vnd.ms-excel, ' + encodeURIComponent(retira_acentos(html)));
+}
+
+
 App.init()
